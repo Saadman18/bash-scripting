b4597e784859918240c1129c0cadd915d747b405
Saadman18 <saadman.sakib.ss@gmail.com>
2022-08-24 01:55:32 +0600

    Countdown Timer

--- countdown.sh
+++ countdown.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-
 # Program that counts down to zero from a given argument
-
-if [[ $1 -le 5 ]]
+echo -e "\n~~ Countdown Timer ~~\n"
+if [[ $1 -gt 0 ]]
 then
-  echo true
+: '
+  for (( i = $1; i >= 0; i-- ))
+  do
+    sleep 1
+    echo $i
+  done
+  '
+  I=$1
+  while [[ $I -ge 0 ]]
+  do
+   echo $I
+   (( I-- ))
+   sleep 1
+  done
 else
-  echo false
+  echo Include a positive integer as the first argument.
 fi
